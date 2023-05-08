1a6012b988651596a3abf9d9674202faa6050b51
HanSeongLee <jkhanseong@gmail.com>
2023-05-08 19:36:38 +0900

    ADD | Font

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+    weight: ['400', '600'],
+    subsets: ['latin'],
+    variable: '--inter-font',
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <div className={inter.className}>
+            <Component {...pageProps} />
+        </div>
+    );
 }
 
-export default MyApp
+export default MyApp;
