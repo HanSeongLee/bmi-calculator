456d8b92a8f6c76dd4aece868e5c886f9d669cd3
HanSeongLee <jkhanseong@gmail.com>
2023-05-23 23:21:04 +0900

    UPDATE | IntroSection component on tablet

--- components/sections/IntroSection/index.tsx
+++ components/sections/IntroSection/index.tsx
@@ -12,21 +12,28 @@ const IntroSection: React.FC<IProps> = ({ className, ...props }) => {
         <section className={cn(styles.introSection, className)}
                  {...props}
         >
-            <img className={styles.image}
-                 src={'/img/image-man-eating.webp'}
-                 alt={''}
-            />
             <Container className={styles.container}>
-                <h2 className={styles.title}>
-                    What your BMI result means
-                </h2>
-                <p className={styles.description}>
-                    A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may lower
-                    your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a
-                    nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables.
-                    Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a
-                    week.
-                </p>
+                <div className={styles.imageWrapper}>
+                    <img className={styles.image}
+                         src={'/img/image-man-eating.webp'}
+                         alt={''}
+                    />
+                </div>
+                <div className={styles.content}>
+                    <h2 className={styles.title}>
+                        What your BMI result means
+                    </h2>
+                    <p className={styles.description}>
+                        A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may
+                        lower
+                        your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim
+                        for a
+                        nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables.
+                        Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days
+                        a
+                        week.
+                    </p>
+                </div>
             </Container>
         </section>
     );
