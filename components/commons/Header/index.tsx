be4fbc19a7c46fb82e5fdf76581e4c9f52535016
HanSeongLee <jkhanseong@gmail.com>
2023-05-27 16:09:44 +0900

    UPDATE | Header component on destkop

--- components/commons/Header/index.tsx
+++ components/commons/Header/index.tsx
@@ -2,6 +2,7 @@ import React, { HTMLAttributes } from 'react';
 import styles from './style.module.scss';
 import cn from 'classnames';
 import Logo from 'components/commons/Logo';
+import Container from 'components/commons/Container';
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
 
@@ -12,7 +13,9 @@ const Header: React.FC<IProps> = ({ className, ...props }) => {
         <header className={cn(styles.header, className)}
                 {...props}
         >
-            <Logo />
+            <Container className={styles.container}>
+                <Logo />
+            </Container>
         </header>
     );
 };
