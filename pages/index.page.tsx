c71d310fa0fd1cf0ba38e2dc990768eabeb335ff
HanSeongLee <jkhanseong@gmail.com>
2023-06-02 17:34:17 +0900

    UPDATE | Main page

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,12 +1,27 @@
 import type { NextPage } from 'next'
+import React from 'react';
+import styles from './style.module.scss';
+import HeroSection from 'components/sections/HeroSection';
+import Header from 'components/commons/Header';
+import Container from 'components/commons/Container';
+import IntroSection from 'components/sections/IntroSection';
+import GuidelineSection from 'components/sections/GuidelineSection';
+import LimitationSection from 'components/sections/LimitationSection';
 
 const Home: NextPage = () => {
-  return (
-      <>
-        <main>
-        </main>
-      </>
-  );
-}
+    return (
+        <>
+            <Container>
+                <Header />
+            </Container>
+            <main className={styles.main}>
+                <HeroSection className={styles.heroSection} />
+                <IntroSection className={styles.introSection} />
+                <GuidelineSection />
+                <LimitationSection />
+            </main>
+        </>
+    );
+};
 
 export default Home
